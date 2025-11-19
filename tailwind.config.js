81252b4efe0c02b10975f0974159819c60027270
bellecoding <isabellasantos100@hotmail.com>
2025-11-19 17:20:23 +0100

    rebuilt css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,11 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    "bg-gray-700/50",
+    "border-purple-500",
+    "rounded-full",
+    "shadow-[0_0_12px_rgba(168,85,247,0.6)]",
+    "text-white",
+  ],
 };
