345bcb8c76ce4b5d10d7dbbdb5289dc3a1ecb297
bellecoding <isabellasantos100@hotmail.com>
2025-11-19 17:56:52 +0100

    fixed package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
   safelist: [
-    "nav-active",
-
-    // underline style
-    "border-b-2",
     "border-purple-500",
-
-    // active text/color
+    "border-b-2",
     "text-white",
     "font-semibold",
-
-    // rounded style
     "rounded-full",
-    "px-4",
-    "py-1",
-
-    // hover effects
     "hover:border-purple-500",
-    "hover:text-white",
-
-    // glow
-    "shadow-md",
-    "shadow-purple-500/30",
+    "from-purple-600",
+    "to-orange-500",
   ],
 };
